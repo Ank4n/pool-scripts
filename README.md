91c0eb670c8b4f019c6b618d49c6ae5df1575463
Ankan <ankan.anurag@gmail.com>
2024-06-04 22:48:22 +0200

    init script for migration and slash apply

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scripts for managing Nomination Pools
+
+Run with 
+```bash
+yarn run main -e "wss://polkadot-rpc.dwellir.com" -d false
+```
+
+See [`package.json`](./package.json) to see what's up 🔥🔥.
+
---
+++ src/index.ts
@@ -0,0 +1,84 @@
+// needed as of 7.x series, see CHANGELOG of the api repo.
+import '@polkadot/api-augment';
+import '@polkadot/types-augment';
+
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const MNEMONIC = 'should come from env';
+
+const optionsPromise = yargs(hideBin(process.argv))
+	.option('endpoint', {
+		alias: 'e',
+		type: 'string',
+		default: 'wss://polkadot-rpc.dwellir.com',
+		description: 'the wss endpoint. It must allow unsafe RPCs.',
+		demandOption: true
+	})
+	.option('dry', {
+		alias: 'd',
+		type: 'boolean',
+		default: true,
+		description: 'if dry run is enabled, no transactions will be sent.'
+	}).argv;
+
+async function main() {
+	const options = await optionsPromise;
+	const provider = new WsProvider(options.endpoint);
+	const api = await ApiPromise.create({ provider });
+	const latest = await api.derive.chain.bestNumber();
+	const latest_hash = await api.rpc.chain.getBlockHash(latest);
+	const apiAt = await api.at(latest_hash);
+	// sr25519 keyring
+	const keyring = new Keyring({ type: 'sr25519' });
+	const admin = keyring.createFromUri(`${MNEMONIC}`).address;
+
+	console.log(
+		`****************** Connected to node: ${(await api.rpc.system.chain()).toHuman()} [ss58: ${
+			api.registry.chainSS58
+		}] ******************`
+	);
+
+	// go over all pools.
+	const pool_keys = await apiAt.query.nominationPools.bondedPools.keys();
+	for (const key of pool_keys) {
+		// check for pool migration
+		const should_migrate = await apiAt.call.nominationPoolsApi.pool_needs_delegate_migration(key);
+
+		if (!options.dry && should_migrate) {
+			const do_pool_migrate = api.tx.nominationPools.migrate_pool_to_delegate_stake(key);
+			const hash = await do_pool_migrate.signAndSend(admin);
+			console.log('do_pool_migrate completed with hash', hash.toHex());
+		}
+
+		// check for pool slash
+		const pool_pending_slash = await apiAt.call.nominationPoolsApi.pool_pending_slash(key);
+		console.log(`Pool #${key.toHuman()} has pending slash of ${pool_pending_slash.toHuman()}.`);
+	}
+
+	// go over all pool members.
+	const member_keys = await apiAt.query.nominationPools.poolMembers.keys();
+	for (const key of member_keys) {
+		// check for pool migration
+		const should_migrate_delegation =
+			await apiAt.call.nominationPoolsApi.member_needs_delegate_migration(key);
+
+		if (!options.dry && should_migrate_delegation) {
+			const do_member_migrate = api.tx.nominationPools.migrate_delegation(key);
+			const hash = await do_member_migrate.signAndSend(admin);
+			console.log('do_pool_migrate completed with hash', hash.toHex());
+		}
+
+		const should_slash_member = await apiAt.call.nominationPoolsApi.member_pending_slash(key);
+		if (!options.dry && should_slash_member) {
+			const do_member_slash = api.tx.nominationPools.apply_slash(key);
+			const hash = await do_member_slash.signAndSend(admin);
+			console.log('do_member_slash completed with hash', hash.toHex());
+		}
+	}
+
+	process.exit(0);
+}
+
+main().catch(console.error);

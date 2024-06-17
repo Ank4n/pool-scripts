5a7ee2437dc190cf556403f4b2a47ae7ca11bac7
Ankan <ankan.anurag@gmail.com>
2024-06-18 01:24:03 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-const MNEMONIC = 'should come from env';
-
 const optionsPromise = yargs(hideBin(process.argv))
 	.option('endpoint', {
 		alias: 'e',
 		type: 'string',
-		default: 'wss://polkadot-rpc.dwellir.com',
+		default: 'wss://westend-rpc.dwellir.com',
 		description: 'the wss endpoint. It must allow unsafe RPCs.',
 		demandOption: true
 	})
@@ -32,6 +30,7 @@ async function main() {
 	const apiAt = await api.at(latest_hash);
 	// sr25519 keyring
 	const keyring = new Keyring({ type: 'sr25519' });
+	const MNEMONIC = process.env.DOT_BOT_MNEMONIC;
 	const admin = keyring.createFromUri(`${MNEMONIC}`).address;
 
 	console.log(
@@ -46,10 +45,14 @@ async function main() {
 		// check for pool migration
 		const should_migrate = await apiAt.call.nominationPoolsApi.pool_needs_delegate_migration(key);
 
-		if (!options.dry && should_migrate) {
-			const do_pool_migrate = api.tx.nominationPools.migrate_pool_to_delegate_stake(key);
-			const hash = await do_pool_migrate.signAndSend(admin);
-			console.log('do_pool_migrate completed with hash', hash.toHex());
+		if (should_migrate) {
+			if (options.dry) {
+				console.log(`Pool #${key.toHuman()} needs migration.`);
+			} else {
+				const do_pool_migrate = api.tx.nominationPools.migrate_pool_to_delegate_stake(key);
+				const hash = await do_pool_migrate.signAndSend(admin);
+				console.log('do_pool_migrate completed with hash', hash.toHex());
+			}
 		}
 
 		// check for pool slash
@@ -64,17 +67,25 @@ async function main() {
 		const should_migrate_delegation =
 			await apiAt.call.nominationPoolsApi.member_needs_delegate_migration(key);
 
-		if (!options.dry && should_migrate_delegation) {
-			const do_member_migrate = api.tx.nominationPools.migrate_delegation(key);
-			const hash = await do_member_migrate.signAndSend(admin);
-			console.log('do_pool_migrate completed with hash', hash.toHex());
+		if (should_migrate_delegation) {
+			if (options.dry) {
+				console.log(`Member #${key.toHuman()} needs delegation migration.`);
+			} else {
+				const do_member_migrate = api.tx.nominationPools.migrate_delegation(key);
+				const hash = await do_member_migrate.signAndSend(admin);
+				console.log('do_pool_migrate completed with hash', hash.toHex());
+			}
 		}
 
 		const should_slash_member = await apiAt.call.nominationPoolsApi.member_pending_slash(key);
-		if (!options.dry && should_slash_member) {
-			const do_member_slash = api.tx.nominationPools.apply_slash(key);
-			const hash = await do_member_slash.signAndSend(admin);
-			console.log('do_member_slash completed with hash', hash.toHex());
+		if (should_slash_member) {
+			if (options.dry) {
+				console.log(`Member #${key.toHuman()} has pending slash.`);
+			} else {
+				const do_member_slash = api.tx.nominationPools.apply_slash(key);
+				const hash = await do_member_slash.signAndSend(admin);
+				console.log('do_member_slash completed with hash', hash.toHex());
+			}
 		}
 	}
 

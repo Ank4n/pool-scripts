1f2ce7f13144ac3186ff59f948c5421010bd5cef
Ankan <ankan.anurag@gmail.com>
2024-06-18 02:22:46 +0200

    call into runtime api via rpc state query

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // needed as of 7.x series, see CHANGELOG of the api repo.
 import '@polkadot/api-augment';
 import '@polkadot/types-augment';
-
+import { u8aToHex, hexToNumber } from '@polkadot/util';
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
@@ -41,35 +41,45 @@ async function main() {
 
 	// go over all pools.
 	const pool_keys = await apiAt.query.nominationPools.bondedPools.keys();
-	for (const key of pool_keys) {
-		// check for pool migration
-		const should_migrate = await apiAt.call.nominationPoolsApi.pool_needs_delegate_migration(key);
-
-		if (should_migrate) {
-			if (options.dry) {
-				console.log(`Pool #${key.toHuman()} needs migration.`);
-			} else {
-				const do_pool_migrate = api.tx.nominationPools.migrate_pool_to_delegate_stake(key);
-				const hash = await do_pool_migrate.signAndSend(admin);
-				console.log('do_pool_migrate completed with hash', hash.toHex());
-			}
-		}
-
-		// check for pool slash
-		const pool_pending_slash = await apiAt.call.nominationPoolsApi.pool_pending_slash(key);
-		console.log(`Pool #${key.toHuman()} has pending slash of ${pool_pending_slash.toHuman()}.`);
-	}
+	// for (const key of pool_keys) {
+	// 	// check for pool migration
+	// 	const should_migrate = await api.rpc.state.call(
+	// 		'NominationPoolsApi_pool_needs_delegate_migration',
+	// 		key.toHex()
+	// 	);
+	// 	// const should_migrate = await apiAt.call.nominationPoolsApi.pool_needs_delegate_migration(key);
+	//
+	// 	if (should_migrate) {
+	// 		if (options.dry) {
+	// 			console.log(`Pool #${key.toHuman()} needs migration.`);
+	// 		} else {
+	// 			const do_pool_migrate = api.tx.nominationPools.migrate_pool_to_delegate_stake(key);
+	// 			const hash = await do_pool_migrate.signAndSend(admin);
+	// 			console.log('do_pool_migrate completed with hash', hash.toHex());
+	// 		}
+	// 	}
+	//
+	// 	// check for pool slash
+	// 	const pool_pending_slash = await apiAt.call.nominationPoolsApi.pool_pending_slash(key);
+	// 	console.log(`Pool #${key.toHuman()} has pending slash of ${pool_pending_slash.toHuman()}.`);
+	// }
 
 	// go over all pool members.
 	const member_keys = await apiAt.query.nominationPools.poolMembers.keys();
 	for (const key of member_keys) {
 		// check for pool migration
-		const should_migrate_delegation =
-			await apiAt.call.nominationPoolsApi.member_needs_delegate_migration(key);
+		const keyring = new Keyring();
+		const member_acc = u8aToHex(keyring.decodeAddress(key.toHuman()?.toString()));
+		const should_migrate_delegation = await api.rpc.state.call(
+			'NominationPoolsApi_member_needs_delegate_migration',
+			member_acc
+		);
+		// const should_migrate_delegation =
+		// 	await apiAt.call.nominationPoolsApi.member_needs_delegate_migration(key);
 
 		if (should_migrate_delegation) {
 			if (options.dry) {
-				console.log(`Member #${key.toHuman()} needs delegation migration.`);
+				console.log(`Member ${key.toHuman()} needs delegation migration.`);
 			} else {
 				const do_member_migrate = api.tx.nominationPools.migrate_delegation(key);
 				const hash = await do_member_migrate.signAndSend(admin);
@@ -77,10 +87,20 @@ async function main() {
 			}
 		}
 
-		const should_slash_member = await apiAt.call.nominationPoolsApi.member_pending_slash(key);
+		const member_pending_slash_raw = await api.rpc.state.call(
+			'NominationPoolsApi_member_pending_slash',
+			member_acc
+		);
+
+		const member_pending_slash = api.createType(
+			'Balance',
+			hexToNumber(member_pending_slash_raw.toString())
+		);
+
+		const should_slash_member = member_pending_slash.gtn(0);
 		if (should_slash_member) {
 			if (options.dry) {
-				console.log(`Member #${key.toHuman()} has pending slash.`);
+				console.log(`Member ${key.toHuman()} has pending slash of ${member_pending_slash}.`);
 			} else {
 				const do_member_slash = api.tx.nominationPools.apply_slash(key);
 				const hash = await do_member_slash.signAndSend(admin);
